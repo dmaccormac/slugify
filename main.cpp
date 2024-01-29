b83002bc951c0feef893b7734c671df6bc9e3b24
dmaccormac <155025087+dmaccormac@users.noreply.github.com>
2024-01-29 19:22:13 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,43 +1,42 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
 using namespace std;
 
 // slugify a string:
 // 1. replace whitespaces with '-'
 // 2. remove non alphanumeric chars
 // 3. convert uppercase chars to lower
-string slugify(string input){
+string slugify(string input) {
 
-    for (auto& ch: input) { 
+    for (auto &ch : input) {
         if (ch == ' ')
             ch = '-';
 
         else if (!isalnum(ch))
             ch = '\0';
-        
+
         else if (isupper(ch))
-            ch = tolower(ch); 
-    } 
+            ch = tolower(ch);
+    }
 
     return input;
-
 }
 int main(int argc, char **argv) {
 
     if (argc < 2)
         cout << "USAGE: " << argv[0] << " <FILE ...>" << endl;
 
-    int file_count = argc -1;
+    int file_count = argc - 1;
 
-    for (int i = 1; i <= file_count; i ++)
-    {
-        ifstream infile(argv[i]);
+    for (int i = 1; i <= file_count; i++) {
+        ifstream file(argv[i]);
         string line;
         int line_count = 0;
-        while (getline(infile, line)) 
-            cout << std::setfill('0') << std::setw(2) << ++line_count << "-" << slugify(line) << endl;
-        
+        while (getline(file, line))
+            cout << std::setfill('0') << std::setw(2) << ++line_count << "-"
+                 << slugify(line) << endl;
 
+        file.close();
     }
-        return 0;
+    return 0;
 }
